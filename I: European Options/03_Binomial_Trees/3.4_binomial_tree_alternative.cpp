13dc580e0148207954cdfcd04783d9c60d04b9c7
driftalpha <senneldev@gmail.com>
2026-07-25 21:41:02 +0200

    hi

--- I: European Options/03_Binomial_Trees/3.4_binomial_tree_alternative.cpp
+++ I: European Options/03_Binomial_Trees/3.4_binomial_tree_alternative.cpp
@@ -2,6 +2,7 @@
 #include <cmath>
 #include <type_traits>
 #include <algorithm>
+#include "../Headers/functions.hpp"
 #include "../Headers/black_scholes_struct.hpp"
 
 template <typename S_t, typename K_t, typename r_t, typename sigma_t, typename T_t>
